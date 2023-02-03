ac82821c3af419a3adf8649f366f85262b470948
PolBadWolf <gm.gameoff@gmail.com>
2023-02-03 15:51:01 +0500

    create project

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/*
+ * t2_lenght_meter.cpp
+ *
+ * Created: 03.02.2023 15:49:12
+ * Author : Gumerovmn
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
